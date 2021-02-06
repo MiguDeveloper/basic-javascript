ea9fcc92cd3113b0b54313c3e075dc2de23230a2
MiguDeveloper <miguel.peru.seo@gmail.com>
2021-02-05 22:43:19 -0500

    valor por referencia y por valor

--- valor-referencia/valor-referencia.js
+++ valor-referencia/valor-referencia.js
@@ -32,3 +32,29 @@ console.timeEnd('slice');
 otrasFrutas.push('Mango');
 
 console.table({ frutas, otrasFrutas });
+
+// lo tipo primitivos no se pasan por refencia
+// boolan, null, undefined, Number, String y Symbol
+let str1 = 'nuevo';
+let str2 = str1;
+
+str1 += ' curso';
+console.log(str1);
+console.log(str2);
+
+let num1 = 5;
+let num2 = num1;
+
+num2 += 10;
+
+console.log(num1);
+console.log(num2);
+
+// los Object, Array y function se pasan por referencia
+// es decir que no se asignan valores sino referencias
+const a = [1, 2, 3];
+const b = a;
+a.push(6);
+b.push(5);
+
+console.log(a, b);
