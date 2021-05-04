65249b7b02cca4567cb65efce8e5da6cb6b506d4
MiguDeveloper <miguel.peru.seo@gmail.com>
2021-05-04 11:46:51 -0500

    switch implements

--- 00.Fundamentos/switch.js
+++ 00.Fundamentos/switch.js
@@ -14,3 +14,24 @@ switch (dia) {
     console.log('no es Domingo, lunes o martes');
     break;
 }
+
+const getType = (type) => {
+  switch (type) {
+    case 'error':
+    case 0:
+      return 'Error';
+    case 'success':
+    case 1:
+      return 'success';
+    case 'warning':
+    case 2:
+      return 'warning';
+    case 'info':
+    case 3:
+      return 'info';
+    default:
+      return;
+  }
+};
+
+console.log(getType('info'));
